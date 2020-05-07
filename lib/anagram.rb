8c566c086e5c1ff53170ca33710b4f924585864f
Andres Carvajal <jc1993box@hotmail.com>
2020-05-07 03:20:47 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,5 +5,15 @@ class Anagram
 def initialize(word)
   @word = word
 end
-  
+
+def match(words)
+     anagram = []
+     words.each do |wordy_words|
+       if wordy_words.split("").sort == word.split("").sort
+         anagram << wordy_words
+       end
+     end
+     anagram
+   end
+ end
 end
